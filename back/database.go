4d6df736db219b49fd7c29137a9bc77bde743ffc
Asko T.J. Mattila <asko.mattila@gmail.com>
2021-03-09 12:35:14 +0200

    Endpoints working.

--- back/database.go
+++ back/database.go
@@ -10,12 +10,13 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+// Checks if there is a database file, if not creates one.
 func CheckIfDatabaseExists() bool {
 	_, errOpening := os.Stat("./books.db")
 	if errOpening != nil {
 		log.Printf("No database file books.db in ConnecToDb. Creating one.")
 		if CreateDb() {
-			PopulateDb()
+			CreateTableForDb()
 			return true
 		} else {
 			return false
@@ -26,8 +27,8 @@ func CheckIfDatabaseExists() bool {
 	}
 }
 
+// Create database file if there isn't one.
 func CreateDb() bool {
-	// Create database file if there isn't one.
 	file, errCreating := os.Create("books.db")
 	if errCreating != nil {
 		log.Printf("Error creating database file books.db in ConnectToDb. %s", errCreating)
@@ -39,13 +40,12 @@ func CreateDb() bool {
 	}
 }
 
-// PopulateDb, to have some test data.
-func PopulateDb() bool {
-
-	// Open database
-	database, errOpening := sql.Open("sqlite3", "./books.db")
-	if errOpening != nil {
-		log.Printf("Error with opening the database file in PopulateDb. %s", errOpening)
+// CreateTableForDb, Creates table for database and inserts some test data.
+func CreateTableForDb() bool {
+	// Connect to database
+	database, errDB := ConnectToDb()
+	if !errDB {
+		log.Printf("Error with connection to database in database.go->PopulateDb")
 		return false
 	}
 	// Create table
@@ -62,49 +62,115 @@ func PopulateDb() bool {
 		return false
 	}
 	log.Printf("Succesfully created table in books.db .")
-
-	book := Book{
-		ID:          0,
-		Title:       "ATitle",
-		Author:      "AAuthor",
-		Description: "ADesc",
+	// Add test data for books and switch to turn it off
+	testData := true
+	if testData {
+		book := Book{
+			ID:          0,
+			Title:       "ATitle",
+			Author:      "AAuthor",
+			Description: "ADesc",
+		}
+		AddToDatabase(database, book)
+		book2 := Book{
+			ID:          0,
+			Title:       "BTitle",
+			Author:      "BAuthor",
+			Description: "BDesc",
+		}
+		AddToDatabase(database, book2)
+		book3 := Book{
+			ID:          0,
+			Title:       "CTitle",
+			Author:      "CAuthor",
+			Description: "CDesc",
+		}
+		AddToDatabase(database, book3)
 	}
-	AddToDatabase(book)
-	book2 := Book{
-		ID:          0,
-		Title:       "BTitle",
-		Author:      "BAuthor",
-		Description: "BDesc",
-	}
-	AddToDatabase(book2)
-	book3 := Book{
-		ID:          0,
-		Title:       "CTitle",
-		Author:      "CAuthor",
-		Description: "CDesc",
-	}
-	AddToDatabase(book3)
 	defer database.Close()
 	return true
 
 }
 
-func AddToDatabase(book Book) bool {
-	database, errOpening := sql.Open("sqlite3", "./books.db")
-	if errOpening != nil {
-		log.Printf("Error with opening the database file in AddToDatabase. %s.", errOpening)
-		return false
-	}
-	defer database.Close()
+// Add book to database using Book struct, return true if successfull.
+func AddToDatabase(database *sql.DB, book Book) bool {
+
+	// Adding book data
 	statement, _ := database.Prepare("INSERT INTO books ( title, author, description) VALUES (?, ?, ?)")
 	_, errExec := statement.Exec(book.Title, book.Author, book.Description)
 	if errExec != nil {
 		log.Printf("Error with adding new book to database in AddToDatabase. %s.", errExec)
 		return false
-	} else {
-		log.Printf("Added book %s by %s", book.Title, book.Author)
-		return true
 	}
-	//defer database.Close()
+	log.Printf("Added book %s by %s", book.Title, book.Author)
+	return true
+}
+
+// Get Books from the Database, return Books in struct array, return true for success.
+func GetAllBooksFromDatabase(database *sql.DB) ([]Book, bool) {
+	// Variable array to return books with
+	var returnBooks []Book
+	// Query database
+	row, err := database.Query("SELECT * FROM books ORDER BY author")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
+	for row.Next() {
+
+		//Temporary Book
+		var tempBook Book
+		errDB := row.Scan(&tempBook.ID, &tempBook.Title, &tempBook.Author, &tempBook.Description)
+		if errDB != nil {
+			log.Printf("Problem querying database in GetAllBooksFromDatabase %s", errDB)
+			return nil, false
+		}
+		// Add temporary book to returning books array.
+		returnBooks = append(returnBooks, tempBook)
+
+	}
+	// Return books
+	return returnBooks, true
+}
+
+// Delete from database with id number, returns true for success.
+func DeleteBookFromDatabase(database *sql.DB, idToDelete int) bool {
+	statement, _ := database.Prepare("DELETE FROM books WHERE id = ?")
+	_, errExec := statement.Exec(idToDelete)
+	if errExec != nil {
+		log.Printf("Error with removing book from database in database.go->DeleteBookFromDatabase. %s.", errExec)
+		return false
+	}
+	//log.Printf("Removed book %s by %s", book.Title, book.Author)
 	//return true
+	return true
+}
+
+// Updating book in database, returns true with success.
+func UpdateBookInDatabase(database *sql.DB, bookToUpdate Book) bool {
+
+	statement, _ := database.Prepare("UPDATE books SET title = ?, author = ?, description = ? WHERE id = ?")
+	result, errExec := statement.Exec(bookToUpdate.Title, bookToUpdate.Author, bookToUpdate.Description, bookToUpdate.ID)
+	if errExec != nil {
+		log.Printf("Error updating book in database in database.go->UpdateBookInDatabase. %s.", errExec)
+		return false
+	}
+	// Test if anything changed, 1 = good, anything else is a problem.
+	resNumber, _ := result.RowsAffected()
+	if resNumber == 1 {
+		return true
+	} else {
+		return false
+	}
+}
+
+// Connect to database and return pointer to it and true for success.
+func ConnectToDb() (*sql.DB, bool) {
+	database, errOpening := sql.Open("sqlite3", "./books.db")
+	if errOpening != nil {
+		log.Printf("Error opening the database file in database.go->ConnectToDb. %s.", errOpening)
+		return nil, false
+	}
+
+	return database, true
 }
--- back/functions.go
+++ back/functions.go
@@ -3,8 +3,13 @@ package back
 // Functions for endpoints
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 // Return index.html
@@ -17,20 +22,148 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 // GetAllBooks, return all the books in database
 func GetAllBooks(w http.ResponseWriter, r *http.Request) {
-	log.Printf("GetAllBooks")
+
+	//log.Printf("GetAllBooks")
+	// Set Header Content-Type
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	database, errConn := ConnectToDb()
+	defer database.Close()
+	if !errConn {
+		log.Printf("Error connecting to database in functions.go->GetAllBooks.")
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error")
+	}
+
+	// Get All Books from database
+	returnedBooks, errDB := GetAllBooksFromDatabase(database)
+	if !errDB {
+		log.Printf("Error with receiving books in functions.go->GetAllBooks.")
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error")
+	} else {
+		anon, _ := json.Marshal(returnedBooks)
+		n := len(anon)
+		response := string(anon[:n])
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "%s", response)
+		//log.Print(returnedBooks)
+	}
 }
 
 // UpdateBook, update data on book
 func UpdateBook(w http.ResponseWriter, r *http.Request) {
 	log.Printf("UpdateBook")
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	var tempBook Book
+
+	// Decoding JSON->Book Struct
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	errDecode := dec.Decode(&tempBook)
+	if errDecode != nil {
+		log.Printf("Error decoding json in functions.go->NewBook() %s", errDecode)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	// Get Book ID
+	vars := mux.Vars(r)
+	sesId := vars["book"]
+	// Turn Id to int
+	bookId, errConv := strconv.Atoi(sesId)
+	if errConv != nil {
+		log.Printf("Problem with turning")
+	}
+	// Replacing possible ID from JSON with one of the endpoints.
+	tempBook.ID = bookId
+
+	// Connect to Database
+	database, errConn := ConnectToDb()
+	// Close database connection when not needed anymore
+	defer database.Close()
+	if !errConn {
+		log.Printf("Connection to database failed in functions.go->UpdateBook()")
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error")
+	}
+
+	// Update the book in database
+	errDB := UpdateBookInDatabase(database, tempBook)
+	if !errDB {
+		log.Printf("Connection to database failed in functions.go->UpdateBook()")
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error")
+	} else {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Book updated successfully.")
+	}
 }
 
 // NewBook, add a book to database
 func NewBook(w http.ResponseWriter, r *http.Request) {
-	log.Printf("NewBook")
+	//log.Printf("NewBook()")
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	var tempBook Book
+
+	// Decoding JSON->Book Struct
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	errDecode := dec.Decode(&tempBook)
+	if errDecode != nil {
+		log.Printf("Error decoding json in functions.go->NewBook() %s", errDecode)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	// Connect to Database
+	database, errConn := ConnectToDb()
+	// Close database connection when not needed anymore
+	defer database.Close()
+	if !errConn {
+		log.Printf("Connection to database failed in functions.go->NewBook()")
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error")
+	}
+
+	// Add the book to database
+	errDB := AddToDatabase(database, tempBook)
+	if !errDB {
+		log.Printf("Connection to database failed in functions.go->NewBook()")
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error")
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Book added successfully.")
+
 }
 
 // DeleteBook, delete book from database
 func DeleteBook(w http.ResponseWriter, r *http.Request) {
 	log.Printf("DeleteBook")
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	// Get Book ID
+	vars := mux.Vars(r)
+	sesId := vars["book"]
+	// Turn Id to int
+	bookId, errConv := strconv.Atoi(sesId)
+	if errConv != nil {
+		log.Printf("Problem with turning")
+	}
+	// Connect To Database
+	database, errConn := ConnectToDb()
+	log.Printf("Connected to database")
+	// Close database connection when not needed anymore
+	defer database.Close()
+	if !errConn {
+		log.Printf("Connection to database failed in functions.go->DeleteBook()")
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error")
+	}
+	// Delete book
+	log.Printf("Ready to delete book.")
+	DeleteBookFromDatabase(database, bookId)
+	log.Printf("Returning from deleting book.")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Book removed successfully.")
+
 }
